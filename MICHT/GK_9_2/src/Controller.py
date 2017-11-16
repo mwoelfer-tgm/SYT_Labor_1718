6e35ec63ede75dd52ae72b08af1f2ed2f759579f
Martin Wölfer <mwoelfer01@student.tgm.ac.at>
2017-11-16 09:01:56 +0100

    Updated the protocol and documented the project

--- MICHT/GK_9_2/src/Controller.py
+++ MICHT/GK_9_2/src/Controller.py
@@ -8,18 +8,37 @@ from src.Worker import *
 from src.Model import *
 
 
-class Controller():
-    def __init__(self):
-        #Thread.__init__(self)
-        #self.daemon = True
+def find_worker(id):
+    """
+    static method which returns a worker object which gets found by the identifier
+    :param id: the unique identifier by which the worker gets found
+    :return: a worker object
+    """
+    worker = None
+    for w in Model.get_worker():
+        if w.get_id() == id:
+            return w
+    if worker == None:
+        print("This worker doesn't exist, please choose another one!")
 
+class Controller():
+    """
+    The controller handles the instances of the classes the user input.
+    """
+    def __init__(self):
+        # at the beginning 10 workers get added
         for i in range(0,10):
             self.add_worker()
 
+        # start the TaskDispatcher thread
         self.td = TaskDispatcher()
         self.td.start()
 
     def add_worker(self):
+        """
+        a worker instance gets created, started and added to the list in the model
+        :return: None
+        """
         w = Worker(Model.get_last_id()+1)
         w.start()
         Model.add_worker(w)
@@ -27,9 +46,19 @@ class Controller():
 
 
     def print_worker_information(self, id):
-        worker = self.find_worker(id)
+        """
+        prints the information of a certain worker
+        :param id: the unique identifier of the worker whose information gets printed
+        :return: None
+        """
+        # find the worker by the unique identifier
+        worker = find_worker(id)
+
+        if worker == None:
+            return
 
 
+        # print the amount of finished tasks, the remaining tasks, the status/load and the time needed for the last task
         print("Information of Worker %d\n"
               "---------------------------\n"
               "Finished tasks: %d\n"
@@ -40,29 +69,43 @@ class Controller():
               (worker.get_id(), worker.get_tasks_finished(), worker.get_tasks_remaining(), worker.get_load() , str(worker.get_time_last_task())))
 
     def print_all_workers(self):
+        """
+        print the information of all workers
+        :return: None
+        """
         for w in Model.get_worker():
             self.print_worker_information(w.get_id())
 
     def delete_worker(self, id):
-        worker = self.find_worker(id)
+        """
+        delete a certain worker
+        :param id: the unique identifier of the worker who gets deleted
+        :return: None
+        """
+        worker = find_worker(id)
+        if worker == None:
+            return
 
         Model.delete_worker(worker)
         print("Worker %d has been deleted" % worker.id)
 
-    def find_worker(self, id):
-        worker = None
-        for w in Model.get_worker():
-            if w.get_id() == id:
-                return w
-        if worker == None:
-            print("This worker doesn't exist, please choose another one!")
-            return
-
     def set_time_interval(self, time):
+        """
+        delegate the parameter to the function of the TaskDispatcher since the time-interval is defined there
+        :param time: the amount of time inbetween each dispatch of a task
+        :return: None
+        """
         self.td.set_time_interval(time)
 
     def disable_enable_worker(self, id):
-        worker = self.find_worker(id)
+        """
+        disable or enable a worker, depening on whether the certain worker is already enabled or disabled
+        :param id: the unique identfier of the worker which gets enabled or disabled
+        :return:
+        """
+        worker = find_worker(id)
+        if worker == None:
+            return
 
         if worker.get_disabled():
             worker.enable()
@@ -70,25 +113,48 @@ class Controller():
             worker.disable()
 
     def disable_enable_logging(self):
+        """
+        disable or enable logging, depending on whether logging has already been enabled or disabled
+        :return: None
+        """
         if Model.is_logging():
             Model.disable_logging()
         else:
             Model.enable_logging()
 
     def set_least_connection(self):
+        """
+        set the current load balancing method to LeastConnection
+        :return: None
+        """
         self.td.set_load_balancer(LeastConnection())
 
     def set_agent_based(self):
+        """
+        set the current load balancing method AgentBasedAdaptiveBalancing
+        :return: None
+        """
         self.td.set_load_balancer(AgentBasedAdaptiveBalancing())
 
 class TaskDispatcher(threading.Thread):
+    """
+    The TaskDispatcher is a thread which delegates a task every x seconds to the LoadBalancer
+    """
     def __init__(self):
         threading.Thread.__init__(self)
+        # since it makes handling closing the application more convenient, the TaskDispatcher is a daemon thread
         self.daemon = True
+        # the standard balancing method is LeastConnection
         self.lb = LeastConnection()
+        # the standard time interval is 0.5
         self.time_interval = 0.5
 
     def run(self):
+        """
+        in a while true loop, call the distribute method of the unknown load balancer with the load
+        the load is a number which gets incremented each time it loops
+        :return: None
+        """
         number = 0
         while(True):
             number += 1
@@ -96,16 +162,25 @@ class TaskDispatcher(threading.Thread):
             time.sleep(self.time_interval)
 
     def set_time_interval(self, time):
+        """
+        :param time: the amount of time inbetween each dispatch of a task
+        :return: None
+        """
         self.time_interval = time
 
     def set_load_balancer(self, lb):
+        """
+        set the load balancer method
+        :param lb: the load balancer object which gets set
+        :return: None
+        """
         self.lb = lb
 
 if __name__ == '__main__':
-
+    # start the controller
     c = Controller()
-    #c.start()
 
+    # create a command line interface for the user to use
     while(True):
         response = input("To add a new Worker, write 'A' \n"
               "To get information of a Worker, write 'I #id' \n"
@@ -116,7 +191,6 @@ if __name__ == '__main__':
               "To enable\disable logging, write 'L'\n"
               "To use the LeastConnection method, write 'LC'\n"
               "To use the AgentBasedAdaptiveBalancing method, write 'AB'\n"
-              "To disable\enable a Worker, write 'D #id'\n"
               "To exit the program, write 'E'\n").upper()
 
         if response == '':
@@ -152,7 +226,7 @@ if __name__ == '__main__':
             try:
                 c.disable_enable_worker(int(response[2:]))
             except ValueError:
-                print("If you want to set the time interval, please specify a valid time with the format 'T #seconds'")
+                print("If you want to disable or enable a worker, please specify a valid id with the format 'D #id'")
         else:
             print("Please give a valid command")
 
